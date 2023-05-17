bb2e377970f44b5f21a824195836e2eef782ef82
azzakyandhika <zakiiws22@gmail.com>
2023-05-17 12:13:40 +0700

    add Outlet Element

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = () => {
   return (
     <div className='w-full flex relative'>
       <Sidebar />
       <div className='flex flex-col w-full'>
         <Navbar />
-        {children}
+        <Outlet />
       </div>
     </div>
   );
