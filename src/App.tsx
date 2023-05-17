d6308241519113f690fd7a8dd630c8d920853909
azzakyandhika <zakiiws22@gmail.com>
2023-05-17 12:03:34 +0700

    update route with createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./layouts/Layout";
-import Home from "./pages/HomePage";
-import Login from "./pages/LoginPage";
-import DashboardPage from "./pages/DashboardPage";
+import { RouterProvider } from "react-router-dom";
+import { route } from "./route";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-        <Route
-          path='/'
-          element={
-            <Layout>
-              <main className='px-5 pb-5 pt-2'>
-                <Routes>
-                  <Route
-                    path='/'
-                    element={<Home />}>
-                    <Route
-                      path='dashboard'
-                      element={<DashboardPage />}
-                    />
-                  </Route>
-                </Routes>
-              </main>
-            </Layout>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={route} />;
 }
 
 export default App;
