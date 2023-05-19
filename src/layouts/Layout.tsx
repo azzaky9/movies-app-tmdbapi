c8ec830cccbdf1842fb986eb5f3f9bb2bec8491b
azzakyandhika <zakiiws22@gmail.com>
2023-05-19 13:04:57 +0700

    add login on location home

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
+import HomePage from "../pages/HomePage";
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <div className='w-full flex relative'>
       <Sidebar />
       <div className='flex flex-col w-full'>
         <Navbar />
-        <Outlet />
+        {location.pathname === "/" ? <HomePage /> : <Outlet />}
       </div>
     </div>
   );
