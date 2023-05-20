96aa851a16510953c39a36a3d28db75df1865212
root <root@devzsh-computer.me>
2023-05-20 13:09:27 +0700

    complete useGenre Hooks

---
+++ src/hooks/useGenre.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState, useCallback } from "react";
+
+export const useGenre = (lists: (string | number)[]) => {
+  const [genreList, setGenreList] = useState<{ id: number; name: string }[]>([]);
+
+  const getGenre = useCallback(async () => {
+    const url =
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=6ec04232daa57ba5165114bab7c10f0c";
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setGenreList(data.genres);
+  }, [genreList]);
+
+  useEffect(() => {
+    getGenre();
+  }, []);
+
+  const genreNames = lists?.map((id) => {
+    const valueFromArr = genreList.find((item) => item.id === id);
+
+    return valueFromArr;
+  });
+
+  return { genreNames };
+};
