07eebc81afa786a434eba31fcdb99e74be8acbbd
zackfedev <zakiiws22@gmail.com>
2023-06-06 09:45:23 +0700

    delete and modified utilites folders

--- src/components/common/utils/index.ts
+++ src/components/common/utils/index.ts
@@ -1,4 +1,3 @@
 export { default as LinkAll } from "./LinkAll";
 export { default as Title } from "./Title";
 export { default as InnerShadow } from "./InnerShadow";
-export { default as Genre } from "./Genre";
