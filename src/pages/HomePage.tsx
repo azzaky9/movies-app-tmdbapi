1c7970289c0430faf3072b11082ffedcefddbf1a
zackfedev <zakiiws22@gmail.com>
2023-06-18 15:34:40 +0700

    adding validating hash logout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,13 @@
 import HomeComponent from "../components/HomeComponent";
+import Logout from "@/components/Logout";
 
-const HomePage = () => {
-  return <HomeComponent />;
+const HomePage = ({ location }: { location: string }) => {
+  return (
+    <>
+      {location === "/logout" ? <Logout /> : null}
+      <HomeComponent />;
+    </>
+  );
 };
 
 export default HomePage;
