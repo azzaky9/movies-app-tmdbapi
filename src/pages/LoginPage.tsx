e4dce4857bb9725d5244374816228ac1b1e3288a
zackfedev <zakiiws22@gmail.com>
2023-06-18 15:35:37 +0700

    adding navigating feature, validating user already login or not

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,16 @@
 import LoginComponent from "../components/LoginComponent";
+import { Navigate } from "react-router-dom";
+import { useAuthenticateRequest } from "@/hooks/useAuthenticate";
 
 const Login = () => {
+  const { getCurrentUser } = useAuthenticateRequest();
+  const user = getCurrentUser();
+  // const navigate = useNavigate();
+
+  if (user) {
+    return <Navigate to='/' />;
+  }
+
   return <LoginComponent />;
 };
 
