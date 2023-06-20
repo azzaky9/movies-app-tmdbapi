1e971232ac276978dfb026677a30a6e71d005b49
zackfedev <zakiiws22@gmail.com>
2023-06-20 11:40:39 +0700

    complete infinite scroll component

--- src/lib/InfiniteScrol.tsx
+++ src/lib/InfiniteScrol.tsx
@@ -1,37 +1,45 @@
 import "./InfiniteScroll.css";
+import { memo } from "react";
+import { useMovies } from "@/hooks/useMovies";
+import { TMoviesResponse } from "@/components/HomeComponent";
 
-const InfiniteScrol = () => {
-  // const { data } = useMovies([
-  //   "https://api.themoviedb.org/3/movie/popular?api_key=6ec04232daa57ba5165114bab7c10f0c&language=en-US&page=1",
-  //   "https://api.themoviedb.org/3/movie/popular?api_key=6ec04232daa57ba5165114bab7c10f0c&language=en-US&page=2",
-  // ]);
+const InfiniteScrol = memo(({ reverse }: { reverse?: boolean }) => {
+  const { data } = useMovies<TMoviesResponse>([
+    "https://api.themoviedb.org/3/movie/popular?api_key=6ec04232daa57ba5165114bab7c10f0c&language=en-US&page=1",
+    "https://api.themoviedb.org/3/movie/popular?api_key=6ec04232daa57ba5165114bab7c10f0c&language=en-US&page=2",
+  ]);
 
-  // console.log(data);
+  const source = data[0]?.results.concat(data[1]?.results);
 
-  // return (
-  //   <div className='max-w-[528px] overflow-hidden relative '>
-  //     <div className='flex animate_infinite--scroll'>
-  //       {data[0]?.results?.map(({ poster_path }) => (
-  //         <img
-  //           src={`https://image.tmdb.org/t/p/original${poster_path}`}
-  //           alt='dummy_preview'
-  //           className='w-[300px] h-[300px]'
-  //         />
-  //       ))}
-  //     </div>
-  //     <div className='flex animate_infinite--scroll'>
-  //       {data[0]?.results?.map(({ poster_path }) => (
-  //         <img
-  //           src={`https://image.tmdb.org/t/p/original${poster_path}`}
-  //           alt='dummy_preview'
-  //           className='w-[300px] h-[300px]'
-  //         />
-  //       ))}
-  //     </div>
-  //   </div>
-  // );
+  const chooseSource = reverse ? [0, 10] : [10, 20];
 
-  return <p>hello</p>;
-};
+  return (
+    <article className='wrapper'>
+      <div className={`marquee ${reverse ? "marquee--reverse" : null}`}>
+        <div className='marquee__group'>
+          {source?.slice(chooseSource[0], chooseSource[1]).map((item) => (
+            <img
+              src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+              alt=''
+              className='rounded-xl'
+            />
+          ))}
+        </div>
+
+        <div
+          aria-hidden='true'
+          className='marquee__group'>
+          {source?.slice(chooseSource[0], chooseSource[1]).map((item) => (
+            <img
+              src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+              alt=''
+              className='rounded-3xl'
+            />
+          ))}
+        </div>
+      </div>
+    </article>
+  );
+});
 
 export default InfiniteScrol;
